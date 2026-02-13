c42aee3ceb42a35a95e359a5f09c8c458a9d6005
nikhitadasari26 <nikhitadasari1@gmail.com>
2026-02-13 15:35:16 +0530

    Backend: Configure node-pg-migrate

---
+++ backend/migration.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+module.exports = {
+  migrationsDir: 'migrations',
+  databaseUrl: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  direction: 'up',
+};
